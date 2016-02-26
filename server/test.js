ba820a7d277c4c055de94875fbc734da5877fd49
Joel Ahn <joelahn123@yahoo.com>
2016-02-26 15:24:12 -0600

    add new web scrape of movies based on books

--- server/test.js
+++ server/test.js
@@ -120,5 +120,307 @@ module.exports = {
           "text": "10-Day Green Smoothie Cleanse"
         }
       }
+    ],
+    "collection2": [
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/136251.Harry_Potter_and_the_Deathly_Hallows",
+          "text": "Harry Potter and the Deathly Hallows (Harry Potter, #7)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/152380.Mary_Poppins",
+          "text": "Mary Poppins (Mary Poppins, #1)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/2657.To_Kill_a_Mockingbird",
+          "text": "To Kill a Mockingbird (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/1.Harry_Potter_and_the_Half_Blood_Prince",
+          "text": "Harry Potter and the Half-Blood Prince (Harry Potter #6)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/6952.Like_Water_for_Chocolate",
+          "text": "Like Water for Chocolate (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/34.The_Fellowship_of_the_Ring",
+          "text": "The Fellowship of the Ring (The Lord of the Rings, #1)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/4671.The_Great_Gatsby",
+          "text": "The Great Gatsby (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/17061.Coraline",
+          "text": "Coraline (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/15881.Harry_Potter_and_the_Chamber_of_Secrets",
+          "text": "Harry Potter and the Chamber of Secrets (Harry Potter, #2)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/3.Harry_Potter_and_the_Sorcerer_s_Stone",
+          "text": "Harry Potter and the Sorcerer's Stone (Harry Potter, #1)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/2.Harry_Potter_and_the_Order_of_the_Phoenix",
+          "text": "Harry Potter and the Order of the Phoenix (Harry Potter #5)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/5.Harry_Potter_and_the_Prisoner_of_Azkaban",
+          "text": "Harry Potter and the Prisoner of Azkaban (Harry Potter, #3)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/17899948-rebecca",
+          "text": "Rebecca (ebook)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/13747.Julie_and_Julia",
+          "text": "Julie and Julia: 365 Days, 524 Recipes, 1 Tiny Apartment Kitchen: How One Girl Risked Her Marriage, Her Job, and Her Sanity to Master the Art of Living (Hardcover)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/11713.The_English_Patient",
+          "text": "The English Patient (Hardcover)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/10917.My_Sister_s_Keeper",
+          "text": "My Sister's Keeper (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/4667024-the-help",
+          "text": "The Help (Hardcover)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/25110940-strangers-on-a-bridge",
+          "text": "Strangers on a Bridge: The Case of Colonel Abel and Francis Gary Powers (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/11127.The_Chronicles_of_Narnia",
+          "text": "The Chronicles of Narnia (Chronicles of Narnia, #1-7)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/939559.Aliens",
+          "text": "Aliens (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/35125.Alien",
+          "text": "Alien (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/7604.Lolita",
+          "text": "Lolita (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/25354.Bastard_Out_of_Carolina",
+          "text": "Bastard Out of Carolina (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/2493.The_Time_Machine",
+          "text": "The Time Machine (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/11.The_Hitchhiker_s_Guide_to_the_Galaxy",
+          "text": "The Hitchhiker's Guide to the Galaxy (Hitchhiker's Guide to the Galaxy, #1)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/17214.Starship_Troopers",
+          "text": "Starship Troopers (Mass Market Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/7763.The_Joy_Luck_Club",
+          "text": "The Joy Luck Club (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/133266.Four_Past_Midnight",
+          "text": "Four Past Midnight (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/10585.Insomnia",
+          "text": "Insomnia (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/16735.The_Prince_of_Tides",
+          "text": "The Prince of Tides (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/17470674-fahrenheit-451",
+          "text": "Fahrenheit 451 (Hardcover)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/137791.Divine_Secrets_of_the_Ya_Ya_Sisterhood",
+          "text": "Divine Secrets of the Ya-Ya Sisterhood (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/18131.A_Wrinkle_in_Time",
+          "text": "A Wrinkle in Time (A Wrinkle in Time Quintet, #1)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/161846.The_Midwich_Cuckoos",
+          "text": "The Midwich Cuckoos (Mass Market Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/89717.The_Haunting_of_Hill_House",
+          "text": "The Haunting of Hill House (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/32049.Lady_Chatterley_s_Lover",
+          "text": "Lady Chatterley's Lover (Hardcover)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/18007564-the-martian",
+          "text": "The Martian (Hardcover)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/31463.Far_from_the_Madding_Crowd",
+          "text": "Far from the Madding Crowd (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/52635.The_Hellbound_Heart",
+          "text": "The Hellbound Heart (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/8664353-unbroken",
+          "text": "Unbroken: A World War II Story of Survival, Resilience, and Redemption (Hardcover)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/11720382-catch-me",
+          "text": "Catch Me (Detective D.D. Warren, #6)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/37034.The_Woman_in_Black",
+          "text": "The Woman in Black (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/2376467.Pitch_Perfect",
+          "text": "Pitch Perfect: The Quest for Collegiate A Cappella Glory (Hardcover)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/21920693-the-drop",
+          "text": "The Drop (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/251688.Breakfast_at_Tiffany_s",
+          "text": "Breakfast at Tiffany's (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/18111281-a-long-way-home",
+          "text": "A Long Way Home (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/6791212-the-lost-child-of-philomena-lee",
+          "text": "The Lost Child of Philomena Lee: A Mother, Her Son and a 50 Year Search (Paperback)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/18505779-50-children",
+          "text": "50 Children: One Ordinary American Couple's Extraordinary Rescue Mission into the Heart of Nazi Germany (Hardcover)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/256683.City_of_Bones",
+          "text": "City of Bones (The Mortal Instruments, #1)"
+        }
+      },
+      {
+        "movie_books": {
+          "href": "https://www.goodreads.com/book/show/1845.Into_the_Wild",
+          "text": "Into the Wild (Paperback)"
+        }
+      }
     ]
   }
